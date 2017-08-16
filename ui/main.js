808a8f7dc6e519d1e3be63c36aa35935f53765ba
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-15 23:33:55 -0700

    Client Side JS
    Worked locally to use Client side JS

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+//Change text on click
+var elem = document.getElementById('main-text');
+elem.innerHTML = 'New Value';
+var img = document.getElementById('madi');
+var leftMargin = 0;
+
+ img.onclick= function(){
+	var intvl = setInterval(moveRight,50);
+ }
+ function moveRight(){
+	 leftMargin += 1;
+	 img.style.marginLeft = leftMargin + 'px';
+ }
+	
