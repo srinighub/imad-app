9cd878bf31aec8ec77ec7fd34f18c7f0c03b2b3c
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-16 23:49:41 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 var articles = {
         'article-one': {
             title: 'Srini | Article One',
@@ -88,11 +92,7 @@ app.get('/ui/lion.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'lion.jpg'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
