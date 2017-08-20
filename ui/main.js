f428bfd6164a830c8ae837435b5e917fb9ada02d
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-20 02:20:11 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ button.onclick = function () {
         var names = ['name1','name2','name3'];
         var list = '';
         for (var i=0; i <=names.length, i++){
-            list += '<li>'+ names[i] + '</il>';
+            list += '<li>'+ names[i] + '</li>';
         }
         var ul = document.getElementById("nameList");
         ul.innerHTML = list;
