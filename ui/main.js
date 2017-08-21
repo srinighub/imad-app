9860341c183735c3821bfa765b775143f289e159
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-21 12:37:16 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,33 @@
+//Submit Comment
+var submitComment = document.getElementById("submit_cmnt");
+
+submitComment.onclick = function (){
+    var articleName = document.getElementById("articleName");
+    var articleComment = document.getElementById("articleComment");
+    var readerName = document.getElementById("name").value;
+    
+    //Make the Request to send the value of Inputbox
+    var request = XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            //On Successful execution get the response and store it in a variable
+            if (request.status ==200){
+                var ul = document.getElementById("commentList");
+                ul.innerHTML = request.responseText;
+            }
+        }
+    };
+    
+    //Make the request 
+    var url = 'http://srinikrishnamoorthy.imad.hasura-app.io/articles/'+ articleName.value + '/submit-comment?name=' + readerName + '&comment=' + (articleComment.value).replace(/\n/g, '<br>');
+    request.open("GET", url, true);
+    request.send(null);
+    articleComment.value = '';
+
+};
+
 //API - Counter Code
-window.onload=function()
-{
 
 var button = document.getElementById("counter");
 button.onclick = function () {
@@ -55,32 +82,5 @@ submit.onclick = function (){
 
 };
 
-//Submit Comment
-var submitComment = document.getElementById("submit_cmnt");
 
-submitComment.onclick = function (){
-    var articleName = document.getElementById("articleName");
-    var articleComment = document.getElementById("articleComment");
-    var readerName = document.getElementById("name").value;
-    
-    //Make the Request to send the value of Inputbox
-    var request = XMLHttpRequest();
-    
-    request.onreadystatechange = function () {
-        if (request.readyState == XMLHttpRequest.DONE) {
-            //On Successful execution get the response and store it in a variable
-            if (request.status ==200){
-                var ul = document.getElementById("commentList");
-                ul.innerHTML = request.responseText;
-            }
-        }
-    };
-    
-    //Make the request 
-    var url = 'http://srinikrishnamoorthy.imad.hasura-app.io/articles/'+ articleName.value + '/submit-comment?name=' + readerName + '&comment=' + (articleComment.value).replace(/\n/g, '<br>');
-    request.open("GET", url, true);
-    request.send(null);
-    articleComment.value = '';
 
-};
-}
