e26b19150a55137be51733ecd7f44528a36856eb
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-22 01:33:56 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool =require('pg').Pool;
 var config = {
     user: 'srinikrishnamoorthy',
     database: 'srinikrishnamoorthy',
-    host: 'db.imad.hosura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
