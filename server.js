43be03336b40d418eaf8d5666046599133046d3c
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-22 01:17:51 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool =require('pg').Pool;
+var config = {
+    user:'srinikrishnamoorthy',
+    database: 'srinikrishnamoorthy',
+    host: 'db.imad.hasura.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -97,6 +106,18 @@ function createArticleTemplate(data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  pool.query('select * from article', function(err,result){
+      if (err){
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+  } )
+});
+
 
 app.get('/article/:articleName', function (req, res) {
     // input is articleName = article-one
