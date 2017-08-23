6c1bde4f92f5022d9a69024608be3d30a081d2c4
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-23 02:24:38 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var crpto = require('crypto');
+var crypto = require('crypto');
 
 var config = {
     user: 'srinikrishnamoorthy',
