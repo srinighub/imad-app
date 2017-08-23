1bb72eaf7a1ac42d76bf407a8fdc4f3d73cf799e
srinighub <30948764+srinighub@users.noreply.github.com>
2017-08-23 02:26:58 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/counter', function (req, res) {
 });
 
 function hash(input,salt){
-    var hashed = crypto.pbkdf2Sync(input,'salt', 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString(hex);
 }
 
